415f3d72f82101078e2a0e6606b0c246ebc39999
Camilo Beltran <cambelf.dev@gmail.com>
2021-08-12 15:24:47 -0500

    He agregado la clase de closure

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,10 @@
 import MediaPlayer from "./MediaPlayer";
+
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video }); // instanciando el objeto.
 button.onclick = () => {
+  // cada vez que se de clic al boton el reproductor ejecutara togglePlay()
   player.togglePlay();
 };
