e3189be6e489b5c706e3220677b0a6a7a63c95cb
Camilo Beltran <cambelf.dev@gmail.com>
2021-08-13 09:47:40 -0500

    agregado codigo de creacion primer plugin

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,16 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
+const muteButton = document.querySelector("#muteButton");
 
-const player = new MediaPlayer({ el: video }); // instanciando el objeto.
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] }); // instanciando el objeto.
 button.onclick = () => {
   // cada vez que se de clic al boton el reproductor ejecutara togglePlay()
   player.togglePlay();
+};
+
+muteButton.onclick = () => {
+  player.toggleMute();
 };
