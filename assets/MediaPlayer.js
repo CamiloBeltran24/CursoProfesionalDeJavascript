89c873eae1921fed83c7576ad839d62501b44776
Camilo Beltran <cambelf.dev@gmail.com>
2021-09-16 10:52:00 -0500

    agregada la clase de Abort-fetch, y he creado el archivo de Intersection Observer

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -16,20 +16,16 @@ MediaPlayer.prototype._initPugins = function () {
       this.media.muted = value;
     },
   };
-  // player.muted();
   this.plugins.forEach((plugin) => {
     plugin.run(player);
   });
 };
-
 MediaPlayer.prototype.play = function () {
   this.media.play();
 };
-
 MediaPlayer.prototype.pause = function () {
   this.media.pause();
 };
-
 MediaPlayer.prototype.togglePlay = function () {
   if (this.media.paused) {
     this.play();
@@ -37,15 +33,12 @@ MediaPlayer.prototype.togglePlay = function () {
     this.pause();
   }
 };
-
 MediaPlayer.prototype.mute = function () {
   this.media.muted = true;
 };
-
 MediaPlayer.prototype.unmute = function () {
   this.media.muted = false;
 };
-
 MediaPlayer.prototype.toggleMute = function () {
   if (this.media.muted) {
     this.unmute();
